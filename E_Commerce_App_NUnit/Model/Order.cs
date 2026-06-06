b3a5d2f322a061944ab7b25d25b814bbd818766b
vjcybertech <findvijay112005@gmail.com>
2026-06-06 08:52:28 +0530

    Initial NUnit E-Commerce application

---
+++ E_Commerce_App_NUnit/Model/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_Commerce_App_NUnit.Model
+{
+    public class Order
+    {
+        public decimal ProductPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
